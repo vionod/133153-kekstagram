ee53686623be9c7d195635034ca54ae1b266531e
vionod <vio-nod@mail.ru>
2016-03-31 00:36:19 +0300

    Урок 1, Задание 5
    Исправлена стилистика кода

--- build/js/check.js
+++ build/js/check.js
@@ -1,47 +1,45 @@
 
-function square(a,b) {
-		var result= 0;
-		for (var i=0; (i<a.length)&&(i<b.length); i++) {
-				result=result+(a[i]*b[i]);
+function square(a, b) {
+	  var length = Math.min(a.length, b.length); 
+	  var result = 0;
+	
+		for (var i = 0; i < length; i++) {
+				result  =result + (a[i] * b[i]);
 		};
-		return(result);
+		return result;
 };
 
 function sum(a) {
-		var result=0;
-		for(var i=0; (i<a.length); i++) {
-				result=result+a[i];
+		var result = 0;
+	
+		for(var i = 0; i < a.length; i++) {
+				result = result + a[i];
 		};
-		return(result);
+		return result;
 };
 
 function getMessage(a, b) {
-		var outString='';
-  if (typeof(a)=='boolean') {
+	
+  if (typeof(a) == 'boolean') {
 			  if (a) {
-            outString=('Переданное изображение анимированно и содержит '+ b + ' кадров');
-						return(outString);
-            }
-        else {
-            outString=('Переданное GIF-изображение не анимированно');
-						return(outString);
-            }
+            return 'Переданное изображение анимированно и содержит '+ b + ' кадров'};
+		
+        return 'Переданное GIF-изображение не анимированно';
+            
     }
 	else
-				if (typeof(a)=='number') {
-            outString=('Переданное SVG-изображение содержит ' + a + ' объектов и '+ (b*4)+' атрибутов');
-						return(outString);
+				if ( (typeof(a) == 'number') &&  !isNaN(a) ) {
+            return 'Переданное SVG-изображение содержит ' + a + ' объектов и '+ (b*4)+' атрибутов';
             }
-        else 
-						if ((a.length>1)&&(b.length>1)) {
-								outString=('Общая площадь артефактов сжатия: ' + square(a, b) +' пикселей');
-								return(outString);
-             }
-		//Не могу понять почему зависает при анализе  png?? 
-						else  if (a.length>1) {
-								outString=('Количество красных точек во всех строчках изображения: '+ sum(a));
-								return(outString);
+	
+				if (Array.isArray(a)) {
+						if (Array.isArray(b)) {
+						  return 'Общая площадь артефактов сжатия: ' + square(a, b) + ' пикселей';
+						};
+						
+						return 'Количество красных точек во всех строчках изображения: ' + sum(a);
 						} 
+	return '';
               
   };
 
