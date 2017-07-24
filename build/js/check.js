71418074b602add883c85c590266f4e709401c52
Valeriy Evdokimov <valeriy.evdokimov@gmail.com>
2017-07-24 10:09:33 +0300

    Модуль 1, задание 2
    Подключил скрипты,
    Написал скрипт проверки

---
+++ build/js/check.js
@@ -0,0 +1,25 @@
+var getMessage = function (a, b) {
+
+if (typeof a == "boolean") {
+  if (a) {
+    return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+  } else {
+    return "Переданное GIF-изображение не анимировано";
+  }
+} else if (typeof a == "number") {
+  return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
+} else if (Array.isArray(a) && !Array.isArray(b)) {
+  var sum = 0;
+  for (var i=0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return "Количество красных точек во всех строчках изображения: " + sum;
+} else if (Array.isArray(a) && Array.isArray(b)) {
+  var square = 0;
+  for (var i=0; i < a.length && i < b.length; i++) {
+    square += a[i] * b[i];
+  }
+  return "Общая площадь артефактов сжатия: " + square + " пикселей";
+}
+
+}
